72010cab291b4c204988470df0e10c1e22f04b87
z4na14 <zanajorgesaghin@gmail.com>
2025-02-20 22:02:38 +0100

    Finished first file

--- ssoo_p1_initial_code/crear.c
+++ ssoo_p1_initial_code/crear.c
@@ -7,26 +7,36 @@
 #include <string.h>
 
 
-
 int main(int argc, char *argv[]) {
+
+  // Check if the appropiate number of arguments are used
   if (argc != 3)
   {
     perror("Wrong usage of program");
     exit(-1);
   }
 
-  const char* name = argv[1];
+  // Store name into a variable
+  const char *name = argv[1];
 
+  // Check if the file already exists, otherwise create it
   int fd;
   if ((fd = open(name, O_RDONLY)) == -1)
   {
-    if (strlen(argv[2]) != 3)
+    // Store last character read by function strtol anc check if
+    // it coincides with the terminator
+    char *endptr;
+    const mode_t permissions = strtol(argv[2], &endptr, 8);
+
+    if (strlen(argv[2]) != 4 || *endptr != '\0')
     {
       perror("Wrong format of the permissions");
       exit(-1);
     }
-    const mode_t permissions = atoi(argv[2]);
 
+    // Override process umask to use the permission
+    // specified by user
+    umask(0000);
     if ((fd = creat(name, permissions)) < 0) {
       perror("Error when creating file");
       exit(-1);
